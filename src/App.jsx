3e66191858c46c7b496ad7225fbde85c74bf2c34
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2022-10-09 17:35:00 +0000

    add: started search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AllPost, OnePost, Home } from "./container";
+import { AllPost, OnePost, Home, Search } from "./container";
 import './App.scss';
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
           <Route element={<Home />} path="/" exact />
           <Route element={<AllPost />} path="/allposts" />
           <Route element={<OnePost />} path="/:slug" />
+          <Route element={<Search />} path="/search" />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/container/Search/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import sanityClient from '../../../client';
+import { NavBar, Footer } from '../../components';
+
+import './Search.scss';
+
+const Search = () => {
+  return (
+    <div className='app__search'>
+        <h1>Just Checking</h1>
+    </div>
+  )
+}
+
+export default Search
