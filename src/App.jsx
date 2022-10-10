8c5eb07f2dd2bee6440abb1b4fc01630662505a8
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2022-10-10 12:51:51 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { AllPost, OnePost, Home, Search } from "./container";
 import './App.scss';
 
 const App = () => {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route element={<Home />} path="/" exact />
           <Route element={<AllPost />} path="/allposts" />
           <Route element={<OnePost />} path="/:slug" />
           <Route element={<Search />} path="/search" />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
