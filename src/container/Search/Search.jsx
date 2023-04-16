51e6afe1181a641b9c4c13ad62cccd032b639479
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-04-16 13:04:01 +0100

    Search Page completed

--- src/container/Search/Search.jsx
+++ src/container/Search/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavBar } from '../../components';
+import { NavBar, Footer } from '../../components';
 import sanityClient from '../../../client';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -40,24 +40,33 @@ const Search = (props) => {
       <NavBar />
       <div className="app__search">
         <div className="app__search_title">
-          <h2>You Searched: "{searchTerm}"</h2>
+          <h2>You Searched: {searchTerm}</h2>
         </div>
         <div className="app__search_result">
-          {articles && articles.map((article) => (
-            <div key={article.slug.current} className="app__search_result-article">
-              <img src={article.mainImage.asset.url} alt="" />
-              <div className="content">
-                <div className="text">
-                  <h2>{article.title}</h2>
-                  <h4>{article.auhor}</h4>
-                  <p>{article.articleSummary.slice(0, 100)}.....</p>
+          {articles && articles.filter(
+            (article) => article.title.toLowerCase().includes(searchTerm.toLowerCase())
+          ).map((article) => {
+            return (
+              <div key={article.slug.current} className="app__search_result-article">
+                <img src={article.mainImage.asset.url} alt="" />
+                <div className="content">
+                  <div className="text">
+                    <h2>{article.title}</h2>
+                    <h4>{article.auhor}</h4>
+                    <p>{article.articleSummary.slice(0, 100)}.....</p>
+                  </div>
+                  <Link to={`/articles/${article.slug.current}`}>Read Article</Link>
                 </div>
-                <Link to={`/articles/${article.slug.current}`}>Read Article</Link>
               </div>
-            </div>
-          ))}
+            )
+          }
+          )}
+        </div>
+        <div className='app__search_end'>
+          <h4>It seems we have come to the end. No other articles match your search</h4>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
